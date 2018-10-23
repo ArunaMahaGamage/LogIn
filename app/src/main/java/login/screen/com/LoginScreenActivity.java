33eeecf472f9508dfcf6dbb50e049a52f42347c1
unknown <admgbit@gmail.com>
2018-10-23 16:13:33 +0530

    first commit

---
+++ app/src/main/java/login/screen/com/LoginScreenActivity.java
@@ -0,0 +1,27 @@
+package login.screen.com;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+/**
+ * Created by Abhimanoj on 11/3/17.
+ */
+
+public class LoginScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_screen);
+
+        //Hide Action Bar
+        getSupportActionBar().hide();
+
+    }
+
+ public void isValidUser(View view) {
+
+        //Your Code
+    }
+}
